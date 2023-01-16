09dd0d6a18bad59c257002b940928f3ef65e0b00
Lorenzo Giada Goncalves <lorenzo.giadagoncalves@gmail.com>
2023-01-16 19:40:05 +0100

    Initial commit

---
+++ RevisionAnglais.py
@@ -0,0 +1,47 @@
+import random
+
+# Changer les verbes ici, attention à bien garder la forme : ["Présent","Prétérite","Participe Passé","Français"] pour chaques listes
+verbe = [
+    ["win","won","won","gagner"],
+    ["beat","beat","beaten","battre"],
+    ["become","became","become","devenir"],
+    ["come","came","come","venir"],
+    ["run","ran","run","courrir"],
+    ["awake","awoke","awoken","se réveiller"],
+    ["be","was/were","been","être"],
+    ["bear","bore","borne","supporter"],
+    ["begin","began","begun","commencer"],
+    ["bite","bot","bitten","mordre"]
+]
+
+def randVerb():
+        nb = random.randint(1,len(verbe))
+        form = random.randint(1,4)
+        print("")
+        return [Verbform(form,nb),verbe[nb]]
+            
+def Verbform(nbForm,nb):
+      if nbForm == 1:
+          return (verbe[nb][0]+" _____ "+" _____ "+" _____ ")
+      elif nbForm == 2:
+          return (" _____ "+verbe[nb][1]+" _____ "+" _____ ")
+      elif nbForm == 3:
+          return (" _____ "+" _____ "+verbe[nb][2]+" _____ ")
+      else:
+          return (" _____ "+" _____ "+" _____ "+verbe[nb][3])
+    
+print("Version 1.0")
+print("By GIADA GONCALVES L.")
+print("Attention à écrire votre réponse sous la forme suivante : VerbePrésent VerbePrétérite VerbeParticipePassé InfinitifFrançais !".upper())
+print("Pour stopper le programme, entrer la commande \"stop\"")
+
+while True:
+    verb, correct = randVerb()
+    userInput = input(verb).lower()
+    if correct == userInput.split(" "):
+        print("Great Job !")
+    elif userInput == "stop":
+        print("Stopping...")
+        break
+    else:
+        print("Wrong, the right answer is : "+str(correct))
