e4ca4570aa07eda4ffd473f816b1cadab3e35be6
Lorenzo Giada Goncalves <lorenzo.giadagoncalves@gmail.com>
2023-01-24 12:24:07 +0100

    Update RevisionAnglais.py

--- RevisionAnglais.py
+++ RevisionAnglais.py
@@ -1,7 +1,7 @@
 import random
 
-# Changer les verbes ici, attention à bien garder la forme : ["Présent","Prétérite","Participe Passé","Français"] pour chaques listes
 verbeList = [
+#   ["EN_Présent","EN_Prétérite","EN_ParticipePassé","FR_Infinitif"],
     ["win","won","won","gagner"],
     ["beat","beat","beaten","battre"],
     ["become","became","become","devenir"],
@@ -15,34 +15,59 @@ verbeList = [
 ]
 
 vocabularyList = [
-    
+#   ["MotEN","MotFR"],
+    ["gap year","année sabbatique"],
+    ["reward","récompense"],
+    ["travel","voyager"],
+    ["les pays bas","the netherlands"],
+    ["second amendment rights","les droits relatifs au port d'armes"],
 ]
 
 def randVerb():
         nb = random.randint(1,len(verbeList))
-        form = random.randint(1,4)
+        nbform = random.randint(1,4)
+        if nbform == 1:
+            verbe = (verbeList[nb][0]+" _____ "+" _____ "+" _____ ")
+        elif nbform == 2:
+            verbe =  (" _____ "+verbeList[nb][1]+" _____ "+" _____ ")
+        elif nbform == 3:
+            verbe =  (" _____ "+" _____ "+verbeList[nb][2]+" _____ ")
+        else:
+            verbe =  (" _____ "+" _____ "+" _____ "+verbeList[nb][3])
         print("")
-        return [Verbform(form,nb),verbeList[nb]]
-            
-def Verbform(nbForm,nb):
-      if nbForm == 1:
-          return (verbeList[nb][0]+" _____ "+" _____ "+" _____ ")
-      elif nbForm == 2:
-          return (" _____ "+verbeList[nb][1]+" _____ "+" _____ ")
-      elif nbForm == 3:
-          return (" _____ "+" _____ "+verbeList[nb][2]+" _____ ")
-      else:
-          return (" _____ "+" _____ "+" _____ "+verbeList[nb][3])
- 
+        return [verbe,verbeList[nb]]
+
+def randVoc():
+    nb = random.randint(1,len(vocabularyList))
+    
+
 print("Pour stopper le programme, entrer la commande \"stop\"")
 
+choice = None
 while True:
-    verb, correct = randVerb()
-    userInput = input(verb).lower()
-    if correct == userInput.split(" "):
-        print("Great Job !")
-    elif userInput == "stop":
-        print("Stopping...")
-        break
-    else:
-        print("Wrong, the right answer is : "+str(correct))
+    print("Que voulez vous travailler ?")
+    choice = input("Verbes, Vocabulaire")
+    if choice.lower() == "verbe":
+        while True:
+            verbe, correct = randVerb()
+            userInput = input(verbe).lower()
+            if correct == userInput.split(" "):
+                print("Great Job !")
+            elif userInput == "stop":
+                print("Stopping...")
+                break
+            else:
+                print("Wrong, the right answer is : "+str(correct))
+        
+        
+    elif choice.lower == "vocabulaire":
+        while True:
+            
+            userInput = input(verbe).lower()
+            if correct == userInput.split(""):
+                print("Great Job !")
+            elif userInput == "stop":
+                print("Stopping...")
+                break
+            else:
+                print("Wrong, the right answer is : "+str(correct))
